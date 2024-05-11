88753e07827182d6e32bc5b4545fb386bdf13617
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-05-11 11:29:18 +0100

    module doc

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""1. Simple pagination
+"""
 import csv
 import math
 from typing import List, Tuple
--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+"""2. Hypermedia pagination
+"""
 import csv
 import math
 from typing import List, Tuple
